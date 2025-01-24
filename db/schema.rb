12e90f3f4c2bb31cf9c4dc7ed7bad4028af4ab6b
Joseph Lee <jhjlee702@gmail.com>
2025-01-23 20:36:29 -0700

    create watch model and spec file

---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2025_01_24_033402) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "watches", force: :cascade do |t|
+    t.string "reference_number"
+    t.string "model"
+    t.integer "price"
+    t.string "movement"
+    t.integer "year_of_production"
+    t.string "case_material"
+    t.string "case_diameter"
+    t.string "description"
+    t.string "bracelet"
+    t.string "lug_to_lug"
+    t.boolean "lume"
+    t.bigint "brand_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_watches_on_brand_id"
+  end
+
+  add_foreign_key "watches", "brands"
+end
