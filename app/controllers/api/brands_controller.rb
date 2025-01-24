5901fd28605ababdabaee56a331b62b183eff599
Joseph Lee <jhjlee702@gmail.com>
2025-01-23 20:52:23 -0700

    create brands_controller endpoint and bind to api route

---
+++ app/controllers/api/brands_controller.rb
@@ -0,0 +1,8 @@
+module Api
+  class BrandsController < ApplicationController
+    def index
+      @brands = Brand.all
+      render json: @brands
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_24_033402) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_24_033730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
     t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email_address"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
