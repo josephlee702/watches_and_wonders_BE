32dc4305bc46a37be2b7f5c82999b292f0c605b1
Joseph Lee <jhjlee702@gmail.com>
2025-01-27 15:18:59 -0700

    add validations to models

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,6 @@
 class Brand < ApplicationRecord
   has_many :watches, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+  validates :country, presence: true
 end
--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,3 +1,6 @@
 class Watch < ApplicationRecord
   belongs_to :brand
+  
+  validates :reference_number, presence: true, uniqueness: true
+  validates :model, :price, :movement, :year_of_production, :case_material, :case_diameter, presence: true
 end
