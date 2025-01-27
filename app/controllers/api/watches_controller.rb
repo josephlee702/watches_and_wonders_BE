b2521d48f25c17459e25027260405dc942ec0c9f
Joseph Lee <jhjlee702@gmail.com>
2025-01-27 14:51:34 -0700

    still looking for appropriate api - pushing back implementation of 3PAPI for now

--- app/controllers/api/watches_controller.rb
+++ app/controllers/api/watches_controller.rb
@@ -1,8 +1,6 @@
-module Api
-  class WatchesController < ApplicationController
-    def index
-      @watches = Watch.all
-      render json: @watches
-    end
+class Api::WatchesController < ApplicationController
+  def index
+    # @watches = WatchApiService.get_watch_data
+    # render json: @watches
   end
 end
---
+++ app/services/watch_api_service.rb
@@ -0,0 +1,12 @@
+class WatchApiService
+  def self.get_watch_data
+    # conn = Faraday.new(url: "https://rapidapi.com")
+    # response = conn.get("/watches")
+
+    # if response.status == 200
+    #   JSON.parse(response.body)
+    # else
+    #   { error: "Unable to fetch watch data. Status code is #{response.status}." }
+    # end
+  end
+end
